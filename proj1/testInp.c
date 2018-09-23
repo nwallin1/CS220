af8de237a5924f337f28a042324ddd6cee9c1ca4
Nicholas Walling <nwallin1@cs.binghamton.edu>
2018-09-23 17:10:16 -0400

    update

--- proj1/testInp.c
+++ proj1/testInp.c
@@ -17,12 +17,5 @@ int main(int x, char **inp){
 	//instantiate players, hand, and cards
 	Hand * hand = malloc(sizeof(Hand));
 	Player p1, p2, p3;
-	hand->players[0] = p1; hand->players[1] = p2; hand->players[2] = p3;
-	//FILE *fp = fopen(inp[1], "r"); //fp points to input file
-	char line[256];
-	/*while(fgets(line, 255, fp)){
-		//EX Line: QD 6H 8S QS 3D 6D 5H AH 4D 2S 7D
-		sscanf(line, "%d %s %d %s", pL.pc1.val, pL.pc1.suite, pL.pc2.val, pL.pc2.suite);
-		printf("Player 1's cards:%d%s + P2 = %d%s\n", pL.pc1.val, pL.pc1.suite, pL.pc2.val, pL.pc2.suite);
-	}*/	
+	hand->players[0] = p1; hand->players[1] = p2; hand->players[2] = p3;	
 }
